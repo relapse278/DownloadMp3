2ce0053ea96552f91ac7e1dc9caf8beadadf0cdb
Руслан Шевченко <r.schewtschenko@mail.ru>
2024-03-14 22:22:56 +0700

    Download all mp3 files from a locally saved html file. Altered InputStream into Scanner.

--- src/DownloadMp3.java
+++ src/DownloadMp3.java
@@ -1,66 +1,86 @@
 import java.io.*;
 import java.net.URL;
-import java.nio.channels.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class DownloadMp3 {
     public static void main(String[] args) {
+            DownloadMp3 program = new DownloadMp3();
+            program.action("C:/test/Фиксики.html");
+    }
 
+    private void action(String filename) {
+        downloadFiles(getUris(getStringFromFile(filename)));
     }
 
     private void downloadFiles(List<String> listOfUris) {
         //          https://www.baeldung.com/java-download-file
-        String FILE_URL = "https://cdn1.101.ru/vardata/modules/podcast/data/2024/03/10/94d6cab1b2dd3a7cbd6ed18dfaa61f44.mp3";
-        String FILE_NAME = "C:/test/test.mp3";
+        int i = 1;
 
-        InputStream in = null;
-        try {
-            in = new URL(FILE_URL).openStream();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            Files.copy(in, Paths.get(FILE_NAME), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        for (String uri : listOfUris) {
+//            String FILE_URL = "https://cdn1.101.ru/vardata/modules/podcast/data/2024/03/10/94d6cab1b2dd3a7cbd6ed18dfaa61f44.mp3";
+            String FILE_NAME = "C:/test/Фиксики" + String.format("%03d", (i++)) + ".mp3";
+
+            InputStream in = null;
+            try {
+                in = new URL(uri).openStream();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Files.copy(in, Paths.get(FILE_NAME), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
-    InputStream getInputStreamFromFile(String fileName) {
-        InputStream inputStream = null;
+    private String getStringFromFile(String fileName) {
+        //    https://stackoverflow.com/questions/224952/most-concise-way-to-read-the-contents-of-a-file-input-stream-in-java
+        Scanner s;
+
         try {
-            File file = new File(getClass().getClassLoader().getResource("fileTest.txt").getFile());
-            inputStream = new FileInputStream(file);
+            s = new Scanner(new File(fileName));
         } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            throw new RuntimeException(e);
         }
+        StringBuilder builder = new StringBuilder();
+        while(s.hasNextLine()) builder.append(s.nextLine());
+
+        return builder.toString();
+        //System.out.println(builder.toString());
     }
 
-    private static String readFromInputStream(InputStream inputStream)
-            throws IOException {
-        StringBuilder resultStringBuilder = new StringBuilder();
-        try (BufferedReader br
-                     = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                resultStringBuilder.append(line).append("\n");
-            }
-        }
-        return resultStringBuilder.toString();
-    }
+//    private String readFromInputStream(InputStream inputStream)
+//            throws IOException {
+//        StringBuilder resultStringBuilder = new StringBuilder();
+//        try (BufferedReader br
+//                     = new BufferedReader(new InputStreamReader(inputStream))) {
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                resultStringBuilder.append(line).append("\n");
+//            }
+//        }
+//        return resultStringBuilder.toString();
+//    }
 
-    private static List<String> getUris(String text) {
-        text.f
+    private List<String> getUris(String text) {
+        //      https://regex101.com/library/qY2kI7?orderBy=MOST_UPVOTES&page=1&search=&filterFlavors=pcre
+        final String regex = "((https:)[A-Z0-9a-z\\.\\/]*(\\.mp3))";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(text);
+        List<String> allMatches = new ArrayList<>();
+
+        while (matcher.find()) {
+            allMatches.add(matcher.group(0));
+        }
+
+        return allMatches.stream().distinct().collect(Collectors.toList());
     }
 }
